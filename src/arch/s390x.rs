d01ef151bcfd73978353282380834931a3253d5e
Taiki Endo <te316e89@gmail.com>
2022-06-15 22:57:54 +0900

    Implement s390x 32-bit and 64-bit swap

--- src/arch/s390x.rs
+++ src/arch/s390x.rs
@@ -1,5 +1,5 @@
 // Generated asm:
-// - s390x https://godbolt.org/z/PP9Wacs7G
+// - s390x https://godbolt.org/z/E3cxcjn7G
 
 use core::{
     arch::asm,
@@ -7,7 +7,7 @@ use core::{
     sync::atomic::{self, Ordering},
 };
 
-use crate::raw::{AtomicLoad, AtomicStore};
+use crate::raw::{AtomicLoad, AtomicStore, AtomicSwap};
 
 macro_rules! atomic_load_store {
     ($int_type:ident, $asm_suffix:tt, $st_suffix:tt) => {
@@ -65,16 +65,53 @@ macro_rules! atomic_load_store {
     };
 }
 
+macro_rules! atomic {
+    ($int_type:ident, $asm_suffix:tt, $st_suffix:tt) => {
+        atomic_load_store!($int_type, $asm_suffix, $st_suffix);
+        impl AtomicSwap for $int_type {
+            #[inline]
+            unsafe fn atomic_swap(
+                dst: *mut MaybeUninit<Self>,
+                val: *const MaybeUninit<Self>,
+                out: *mut MaybeUninit<Self>,
+                _order: Ordering,
+            ) {
+                // SAFETY: the caller must uphold the safety contract for `atomic_swap`.
+                unsafe {
+                    // atomic swap is always SeqCst.
+                    asm!(
+                        // load from val to val_tmp
+                        concat!("l", $asm_suffix, " {val_tmp}, 0({val})"),
+                        // (atomic) swap (cas loop)
+                        concat!("l", $asm_suffix, " %r0, 0({dst})"),
+                        "2:",
+                            concat!("cs", $asm_suffix, " %r0, {val_tmp}, 0({dst})"),
+                            "jl 2b",
+                        // store r0 to out
+                        concat!("st", $st_suffix, " %r0, 0({out})"),
+                        dst = inout(reg) dst => _,
+                        val = in(reg) val,
+                        val_tmp = lateout(reg) _,
+                        out = inout(reg) out => _,
+                        out("r0") _,
+                        options(nostack),
+                    );
+                }
+            }
+        }
+    };
+}
+
 atomic_load_store!(i8, "b", "c");
 atomic_load_store!(u8, "b", "c");
 atomic_load_store!(i16, "h", "h");
 atomic_load_store!(u16, "h", "h");
-atomic_load_store!(i32, "", "");
-atomic_load_store!(u32, "", "");
-atomic_load_store!(i64, "g", "g");
-atomic_load_store!(u64, "g", "g");
-atomic_load_store!(isize, "g", "g");
-atomic_load_store!(usize, "g", "g");
+atomic!(i32, "", "");
+atomic!(u32, "", "");
+atomic!(i64, "g", "g");
+atomic!(u64, "g", "g");
+atomic!(isize, "g", "g");
+atomic!(usize, "g", "g");
 
 // https://github.com/llvm/llvm-project/commit/a11f63a952664f700f076fd754476a2b9eb158cc
 macro_rules! atomic128 {
@@ -144,19 +181,19 @@ atomic128!(u128);
 
 #[cfg(test)]
 mod tests {
-    test_atomic_load_store!(isize);
-    test_atomic_load_store!(usize);
+    test_atomic!(isize);
+    test_atomic!(usize);
     test_atomic_load_store!(i8);
     test_atomic_load_store!(u8);
     test_atomic_load_store!(i16);
     test_atomic_load_store!(u16);
-    test_atomic_load_store!(i32);
-    test_atomic_load_store!(u32);
-    test_atomic_load_store!(i64);
-    test_atomic_load_store!(u64);
+    test_atomic!(i32);
+    test_atomic!(u32);
+    test_atomic!(i64);
+    test_atomic!(u64);
     test_atomic_load_store!(i128);
     test_atomic_load_store!(u128);
 
     stress_test_load_store!();
-    // stress_test_load_swap!();
+    stress_test_load_swap!();
 }
