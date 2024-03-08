7fd1f60f0cf6920721d41b7327052f7cc2827570
Taiki Endo <te316e89@gmail.com>
2024-03-09 02:43:39 +0900

    tools: Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -194,9 +194,9 @@ if [[ "${rustc_version}" == *"nightly"* ]] || [[ "${rustc_version}" == *"dev"* ]
     # The latest syntax of -Z check-cfg requires 1.75.0-nightly.
     # We only check this on the recent nightly to avoid old clippy bugs.
     # shellcheck disable=SC2207
-    if [[ "${rustc_minor_version}" -ge 76 ]]; then
+    if [[ "${rustc_minor_version}" -ge 78 ]]; then
         build_scripts=(build.rs)
-        check_cfg='-Z unstable-options --check-cfg=cfg(target_pointer_width,values("128")) --check-cfg=cfg(target_arch,values("xtensa")) --check-cfg=cfg(target_feature,values("lse2","lse128","rcpc3","v8m","partword-atomics","quadword-atomics","fast-serialization"))'
+        check_cfg='-Z unstable-options --check-cfg=cfg(target_pointer_width,values("128")) --check-cfg=cfg(target_arch,values("xtensa","arm64ec")) --check-cfg=cfg(target_feature,values("lse2","lse128","rcpc3","v8m","partword-atomics","quadword-atomics","fast-serialization"))'
         known_cfgs+=($(grep -E 'cargo:rustc-cfg=' "${build_scripts[@]}" | sed -E 's/^.*cargo:rustc-cfg=//; s/(=\\)?".*$//' | LC_ALL=C sort -u))
         # TODO: handle multi-line target_feature_if
         known_target_feature_values+=($(grep -E 'target_feature_if\("' "${build_scripts[@]}" | sed -E 's/^.*target_feature_if\(//; s/",.*$/"/' | LC_ALL=C sort -u))
