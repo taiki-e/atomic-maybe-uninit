4a50d75307f62b6e33f61cce40c73f01f513577b
Taiki Endo <te316e89@gmail.com>
2025-01-05 15:10:06 +0900

    Update create_sub_word_mask_values

--- src/utils.rs
+++ src/utils.rs
@@ -253,12 +253,14 @@ type RetInt = RegSize;
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn create_sub_word_mask_values<T>(ptr: *mut T) -> (*mut MinWord, RetInt, RetInt) {
-    // RISC-V, MIPS, SPARC, LoongArch, Xtensa: shift amount of 32-bit shift instructions is 5 bits unsigned (0-31).
+    // RISC-V, MIPS, SPARC, LoongArch, Xtensa, BPF: shift amount of 32-bit shift instructions is 5 bits unsigned (0-31).
     // PowerPC, C-SKY: shift amount of 32-bit shift instructions is 6 bits unsigned (0-63) and shift amount 32-63 means "clear".
     // Arm: shift amount of 32-bit shift instructions is 8 bits unsigned (0-255).
     // Hexagon: shift amount of 32-bit shift instructions is 7 bits signed (-64-63) and negative shift amount means "reverse the direction of the shift".
     // (On s390x, we don't use the mask returned from this function.)
+    // (See also https://devblogs.microsoft.com/oldnewthing/20230904-00/?p=108704 for others)
     const SHIFT_MASK: bool = !cfg!(any(
+        target_arch = "bpf",
         target_arch = "loongarch64",
         target_arch = "mips",
         target_arch = "mips32r6",
