6bae0c665ae75e147952450aba3c7d1226c88f24
Taiki Endo <te316e89@gmail.com>
2025-01-16 00:03:06 +0900

    tools: Add gen.sh

---
+++ tools/gen.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+set -CeEuo pipefail
+IFS=$'\n\t'
+trap -- 's=$?; printf >&2 "%s\n" "${0##*/}:${LINENO}: \`${BASH_COMMAND}\` exit with ${s}"; exit ${s}' ERR
+cd -- "$(dirname -- "$0")"/..
+
+# Run code generators.
+#
+# USAGE:
+#    ./tools/gen.sh
+
+set -x
+
+./tools/target_spec.sh
