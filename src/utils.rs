6e465178a68aa562c8ad2e0b2bc62ce56eafb3cb
Taiki Endo <te316e89@gmail.com>
2025-04-25 01:27:39 +0900

    Resolve clippy::cast_possible_truncation warning on s390x
    ```
      error: casting `usize` to `u32` may truncate the value on targets with 64-bit wide pointers
         --> src/utils.rs:334:19
          |
      334 |     (aligned_ptr, shift as RetInt, mask)
          |                   ^^^^^^^^^^^^^^^
          |
          = help: if this is intentional allow the lint with `#[allow(clippy::cast_possible_truncation)]` ...
          = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#cast_possible_truncation
          = note: `-D clippy::cast-possible-truncation` implied by `-D warnings`
          = help: to override `-D warnings` add `#[allow(clippy::cast_possible_truncation)]`
      help: ... or use `try_from` and handle the error accordingly
          |
      334 -     (aligned_ptr, shift as RetInt, mask)
      334 +     (aligned_ptr, RetInt::try_from(shift), mask)
          |
    ```

--- src/utils.rs
+++ src/utils.rs
@@ -331,7 +331,11 @@ pub(crate) fn create_sub_word_mask_values<T>(ptr: *mut T) -> (*mut MinWord, RetI
     if SHIFT_MASK {
         mask <<= shift;
     }
-    (aligned_ptr, shift as RetInt, mask)
+    // ptr_lsb << 3 will never overflow u32
+    #[cfg_attr(target_arch = "s390x", allow(clippy::cast_possible_truncation))]
+    {
+        (aligned_ptr, shift as RetInt, mask)
+    }
 }
 
 // This module provides core::ptr strict_provenance/exposed_provenance polyfill for pre-1.84 rustc.
