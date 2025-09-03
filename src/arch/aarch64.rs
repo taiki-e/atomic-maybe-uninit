fc40953c6af32f63802a356672972c48476d2f2b
Taiki Endo <te316e89@gmail.com>
2025-09-03 20:14:23 +0900

    aarch64: Add comment about seqcst store with FEAT_LRCPC3

--- src/arch/aarch64.rs
+++ src/arch/aarch64.rs
@@ -527,6 +527,11 @@ macro_rules! atomic128 {
                         // SAFETY: cfg guarantee that the CPU supports FEAT_LRCPC3.
                         #[cfg(any(target_feature = "rcpc3", atomic_maybe_uninit_target_feature = "rcpc3"))]
                         Ordering::Release => atomic_store!("il", "", ""),
+                        // LLVM uses store-release (dmb ish; stp); dmb ish, GCC (libatomic) and Atomics ABI Standard
+                        // uses store-release (stilp) without fence for SeqCst store
+                        // (https://github.com/gcc-mirror/gcc/commit/7107574958e2bed11d916a1480ef1319f15e5ffe).
+                        // Considering https://reviews.llvm.org/D141748, LLVM's lowing seems
+                        // to be the safer option here (I'm not convinced that the libatomic's implementation is wrong).
                         #[cfg(any(target_feature = "rcpc3", atomic_maybe_uninit_target_feature = "rcpc3"))]
                         #[cfg(not(any(target_feature = "lse128", atomic_maybe_uninit_target_feature = "lse128")))]
                         Ordering::SeqCst => atomic_store!("il", "dmb ish", ""),
