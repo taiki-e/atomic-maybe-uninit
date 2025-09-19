50f044b8b3212dff724092852e90c80a0218e0f4
Taiki Endo <te316e89@gmail.com>
2025-09-19 22:18:17 +0900

    Update build script for Rust 1.90

--- build.rs
+++ build.rs
@@ -592,8 +592,8 @@ mod version {
         // the rustc version, we assume this is the current version.
         // It is no problem if this is older than the actual latest stable.
         // LLVM version is assumed to be the minimum external LLVM version:
-        // https://github.com/rust-lang/rust/blob/1.89.0/src/bootstrap/src/core/build_steps/llvm.rs#L613
-        pub(crate) const LATEST: Self = Self::stable(89, 19);
+        // https://github.com/rust-lang/rust/blob/1.90.0/src/bootstrap/src/core/build_steps/llvm.rs#L622
+        pub(crate) const LATEST: Self = Self::stable(90, 19);
 
         pub(crate) const fn stable(rustc_minor: u32, llvm_major: u32) -> Self {
             Self {
