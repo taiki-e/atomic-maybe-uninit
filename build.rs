de420f6a374a5916c22898d8ca7c005f8766bcab
Taiki Endo <te316e89@gmail.com>
2026-01-06 16:19:38 +0900

    riscv: riscv_ratified_v2 has been stabilized

--- build.rs
+++ build.rs
@@ -308,8 +308,11 @@ fn main() {
             // https://github.com/gcc-mirror/gcc/commit/11c2453a16b725b7fb67778e1ab4636a51a1217d
             // https://github.com/rust-lang/rust/pull/130877
             let mut zaamo = false;
-            // target_feature "zacas" is unstable and available on rustc side since nightly-2025-02-26: https://github.com/rust-lang/rust/pull/137417
-            if (!version.probe(87, 2025, 2, 25) || needs_target_feature_fallback(&version, None))
+            // target_feature "zacas" is unstable and available on rustc side
+            // since nightly-2025-02-26 (https://github.com/rust-lang/rust/pull/137417),
+            // and stabilized in Rust 1.94 (https://github.com/rust-lang/rust/pull/145948).
+            if (!version.probe(87, 2025, 2, 25)
+                || needs_target_feature_fallback(&version, Some(94)))
                 && version.llvm >= 20
             {
                 // amocas.{w,d,q} (and amocas.{b,h} if zabha is also available)
@@ -317,8 +320,11 @@ fn main() {
                 // available non-experimental since LLVM 20 https://github.com/llvm/llvm-project/commit/614aeda93b2225c6eb42b00ba189ba7ca2585c60
                 zaamo |= target_feature_fallback("zacas", false);
             }
-            // target_feature "zaamo"/"zabha"/"zalrsc" is unstable and available on rustc side since nightly-2024-10-02: https://github.com/rust-lang/rust/pull/130877
-            if (!version.probe(83, 2024, 10, 1) || needs_target_feature_fallback(&version, None))
+            // target_feature "zaamo"/"zabha"/"zalrsc" is unstable and available on rustc side
+            // since nightly-2024-10-02 (https://github.com/rust-lang/rust/pull/130877),
+            // and stabilized in Rust 1.94 (https://github.com/rust-lang/rust/pull/145948).
+            if (!version.probe(83, 2024, 10, 1)
+                || needs_target_feature_fallback(&version, Some(94)))
                 && version.llvm >= 19
             {
                 // amo*.{b,h}
