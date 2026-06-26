f039131f681054d1dd4e6d7fb802d20c1de018ca
Taiki Endo <te316e89@gmail.com>
2026-06-27 03:54:50 +0900

    detect: Pass CpuInfo to _detect by val
    Remove needless load/store.
    https://godbolt.org/z/3YKn5GM5Y

--- src/detect/common.rs
+++ src/detect/common.rs
@@ -40,7 +40,7 @@ pub(crate) fn detect() -> CpuInfo {
     // Note: detect_false cfg is intended to make it easy for developers to test
     // cases where features usually available is not available, and is not a public API.
     if !cfg!(atomic_maybe_uninit_test_detect_false) {
-        _detect(&mut info);
+        info = _detect(info);
     }
     CACHE.store(info.0, Ordering::Relaxed);
     info
