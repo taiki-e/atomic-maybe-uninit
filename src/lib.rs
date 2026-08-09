bc1cc8c528a767cf5b08aa13eae2747b7db34c33
Taiki Endo <te316e89@gmail.com>
2026-08-10 00:08:30 +0900

    Add since to #[deprecated] on fetch_update

--- src/lib.rs
+++ src/lib.rs
@@ -671,7 +671,7 @@ impl<T: Primitive> AtomicMaybeUninit<T> {
     /// An alias for [`try_update`](Self::try_update).
     #[inline]
     #[cfg_attr(debug_assertions, track_caller)]
-    #[deprecated(note = "renamed to `try_update` for consistency")]
+    #[deprecated(note = "renamed to `try_update` for consistency", since = "0.3.21")]
     pub fn fetch_update<F>(
         &self,
         set_order: Ordering,
